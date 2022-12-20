417ac11ae6b0b5295db13bf90534e5b3f2e0300a
anettgor <anetagorowska06@onet.pl>
2022-12-20 18:51:46 +0100

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(galleryItem => {
+  const link = document.createElement('a');
+  gallery.appendChild(link);
+  link.classList.add('gallery__item');
+  link.href = galleryItem.original;
+
+  const newImg = document.createElement('img');
+  newImg.classList.add('gallery__image');
+  newImg.setAttribute('src', galleryItem.preview);
+  newImg.setAttribute('alt', galleryItem.description);
+  link.appendChild(newImg);
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
