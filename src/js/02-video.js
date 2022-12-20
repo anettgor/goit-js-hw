ac5d78661341553e0db92f23d8294235aba0b508
anettgor <anetagorowska06@onet.pl>
2022-12-20 19:51:08 +0100

    task 2 done ?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,27 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
 });
+const localStorage = window.localStorage;
+console.log(localStorage);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
